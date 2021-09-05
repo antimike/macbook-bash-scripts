60540fd677369c88de8d7c7efb48a51b8b18f5ac
Michael Haynes <antihaynes@gmail.com>
2021-09-04 23:41:27 -0500

    Wrapped ssh-start commands in function

--- ssh-config.bash
+++ ssh-config.bash
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Allows use of GPG keys stored in PGP smartcard for SSH
 
-export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
-gpg-connect-agent updatestartuptty /bye
+sshstart() {
+    export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+    gpg-connect-agent updatestartuptty /bye
+}
 
-alias sshstart="source '${BASH_SOURCE[0]}'"
+sshstart >/dev/null
