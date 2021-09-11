f32364ab1a532cfeea7b0d9c81cd3e508ca608dc
Michael Haynes <antihaynes@gmail.com>
2021-09-10 19:00:54 -0500

    Bug fixes in _set_current_list and agenda

--- todolist.sh
+++ todolist.sh
@@ -18,6 +18,17 @@ if ! [ -d "$NOTES_DIR" ]; then
 fi >&2
 
 _set_current_list() {
+    # TODO: 
+    #   - [X] check for null input
+    #   - [X] check format of input (i.e., list basename, file basename, full path,
+    #       - Added `basename` call
+    #   etc.)
+    #   - [X] expose option to suppress file creation if not found
+    local -i create=1
+    case "$1" in
+        -q|--quiet) create=0; shift ;;
+        "") return -1 ;;        # Empty argument
+    esac
     local list="$(awk -v ext="$DEFAULT_EXT" "$(cat <<-AWK
 		{
 		    ext=\$1 ~ "\\\." ? "" : "." ext
@@ -25,44 +36,57 @@ _set_current_list() {
 		    printf("%s%s%s", pre, \$0, ext)
 		} 
 		AWK
-    )" <<<"$1")"
+    )" <<<"$(basename "$1")")"
     local file="${NOTES_DIR}/${list}"
     if ! [ -e "$file" ]; then
-        echo "Create file '${file}'?"
-        read -s -n 1
-        case "$REPLY" in 
-            y|Y)
-                touch "$file"
-                ;;
-            *)
-                echo "Aborting!" >&2 && return -1
-                ;;
-        esac
+        if [ $create -eq 1 ]; then
+            echo "Create file '${file}'?"
+            read -s -n 1
+            case "$REPLY" in 
+                y|Y)
+                    touch "$file" ||
+                        { echo "Could not touch '${file}'!"; return 1; } >&2
+                    ;;
+                *)
+                    { echo "Aborting!"; return 2; } >&2
+                    ;;
+            esac
+        else
+            { echo "File '${file}' not found!"; return 3; } >&2
+        fi
     fi
     if ! [ -r "$file" ]; then
-        echo "Cannot read file for todo list '$list'" >&2
-        return -1
+        { echo "Cannot read file for todo list '$list'!"; return 4; } >&2
     else
         export CURRENT_LIST="$file"
     fi
     return $?
 }
 
+_commit_current_list() {
+    git add "$CURRENT_LIST" &&
+        git commit $(printf -- '-m "%s"' "$@") ||
+        _warn "Failed to commit '${CURRENT_LIST}'"
+}
+
 agenda() {
-    if [ "$1" = "list" ]; then
+    local list="$1" old_list=
+    if [ "$list" = "list" ]; then
         shift; local search="$*"
         find "${NOTES_DIR}" \
             -maxdepth 1 \
             -name "${LIST_PREFIX}*${search}*.${DEFAULT_EXT}" \
             -printf "%f\n"
         return $?
-    elif [ -z "$1" ]; then
-        echo "AGENDA: ${DEFAULT_LIST}"
-    else
-        _set_current_list "$1"
+    elif [ -z "$list" ]; then
+        list="${DEFAULT_LIST}"
+        old_list="$CURRENT_LIST"
     fi
+    _set_current_list "$list"
     local -i status=$?
-    cat "$CURRENT_LIST"
+    echo "AGENDA: ${CURRENT_LIST}" |
+        ${CAT} - "$CURRENT_LIST"
+    _set_current_list "$old_list" 2>/dev/null
     return $status
 }
 
@@ -80,9 +104,12 @@ finish() {
         esac
     done
     shift $(( OPTIND - 1 ))
-    sed "/$*/s/^- \[ \]/- \[X\]/" "$CURRENT_LIST" | 
-        tee "$CURRENT_LIST"
-    cat "$CURRENT_LIST"
+    sed -i "/$*/s/^- \[ \]/- \[X\]/" "$CURRENT_LIST"
+    _commit_current_list \
+        "Finished items matching '$*' from list '${CURRENT_LIST}'" \
+        "$(echo "Items completed:" && grep "$*" "$CURRENT_LIST" |
+                    sed '2,$s/^/	/')" ||
+    ${CAT} "$CURRENT_LIST"
     return $?
 }
 
@@ -91,10 +118,8 @@ to() {
     _set_current_list "$1" && {
         shift 
         printf -- '- [ ] %s\n' "$@" | tee -a "$CURRENT_LIST"
-    } || {
-        echo "Could not set todo list to '${verb}'" >&2
-        return -1
-    }
+        _commit_current_list
+    } || _warn "Could not set todo list to '$1'"
     return $?
 }
 
